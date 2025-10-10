50d8c42e4a07fe03251165b1af04b0ec28f86d34
ankitgujre <ankitgujre374@gmail.com>
2025-10-10 12:35:59 +0530

    Practice

--- SubQueryTUT.sql
+++ SubQueryTUT.sql
@@ -10,3 +10,20 @@ select emp_name from employees where salary > (select avg(salary) from employees
 # 2. List departments whose average employee salary is more than 50,000.
 
 select dept_name from departments where dept_id in (select dept_id from employees group by dept_id having avg(salary) > 50000);
+
+# Display the employee(s) with the highest salary.
+
+SELECT emp_name FROM employees WHERE salary = (SELECT MAX(salary) FROM employees);
+
+# 4. Show names of employees who work in the same department as ‘John’.
+
+SELECT emp_name
+FROM employees
+WHERE dept_id = (
+    SELECT dept_id
+    FROM employees
+    WHERE emp_name = 'John'
+)
+AND emp_name = 'John';
+
+
---
+++ classPractice.sql
@@ -0,0 +1,53 @@
+use dummyClass;
+select * from customers;
+select * from emp;
+select * from employees;
+select * from offices;
+select * from orderdetails;
+select * from orders;
+select * from payments;
+select * from productlines;
+select * from products;
+
+# 1 Retrieve the customers whose creditLimit is above the average credit limit of all customers.
+
+select customerName from customers where creditLimit > (select avg(creditLimit) from customers);
+
+#using join 
+
+# 2️ Find the customer(s) with the highest credit limit using a subquery.
+
+select customerName from customers where creditLimit = (select max(creditLimit) from customers);
+
+# 3 Get the customers who have placed at least one order (assuming orders table has a customerNumber foreign key).
+
+select c.customerName, c.customerNumber from customers c where customerNumber in (select customernumber from orders);
+
+# 4 Find the customers who haven't made any payments (assuming payments table exists).
+
+# using join 
+select c.customerName
+from customers c
+left join payments p
+on c.customerNumber = p.customerNumber
+where p.amount is null;
+
+
+select customerName from customers where customerNumber not in (select customerNumber from payments);
+
+
+# #5 Get the customers whose credit limit is higher than any customer's credit limit from "New York";.
+
+select customerName from customers where creditlimit > (select max(creditlimit) from customers where city = 'New York');
+
+select customerName from customers where creditlimit > (select max(creditlimit) from customers where city = 'Nantes');
+
+# 6 Find the customer(s) who placed the most orders.
+
+select c.customerName, COUNT(o.orderNumber) as total_orders
+from customers c
+join orders o on c.customerNumber = o.customerNumber
+group by c.customerName
+order by total_orders desc
+LIMIT 1;
+
